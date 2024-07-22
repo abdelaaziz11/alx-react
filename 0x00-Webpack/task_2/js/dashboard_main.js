b2a5129028408cf584b2bdc253d1e1c7ea279820
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-22 17:20:16 +0100

    Adding CSS && images

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,19 +1,20 @@
-import $ from 'jquery';
-import _ from 'lodash';
 import '../css/main.css';
+const $ = require('jquery');
+const _ = require('lodash');
 
-$('body').append('<div id="logo"></div>');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="clickMe">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
+const updateCounter = () => {
+	let times = $('#count').html() || 0;
+	$('button').on('click', () => {
+		times++;
+		$('#count').html(`${times} clicks on the button`);
+	});
+};
 
-function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-}
-
-$('#clickMe').on('click', _.debounce(updateCounter, 500));
+_.debounce(updateCounter, 500);
+updateCounter();
