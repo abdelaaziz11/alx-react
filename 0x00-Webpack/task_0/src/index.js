5e7ef3ba0a56fc884b12332314fabae66498a0e4
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-22 15:30:51 +0100

    basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,6 @@
-import $ from 'jquery';
+import 'jquery';
 
+var $ = require( "jquery" );
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<p>Copyright - Holberton School</p>');
