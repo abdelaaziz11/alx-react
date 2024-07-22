b0c6381f108027e0977da8fb65debfaaf3adab46
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-07-22 16:50:56 +0100

    Adding CSS && images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    performance: {
+        hints: false
+    }
+};
