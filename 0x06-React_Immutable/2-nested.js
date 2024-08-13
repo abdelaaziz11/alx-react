098fc7b8e2d60adf2d3d194f344f484023525e90
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-08-13 17:16:07 +0100

    Accessing nested elements

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+const { fromJS } = require('immutable');
+
+// The function should return the value of the object at the defined path
+export default function accessImmutableObject(object, array) {
+    const immutableObject = fromJS(object);
+    return immutableObject.getIn(array);
+}
