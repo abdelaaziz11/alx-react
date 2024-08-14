9d427f6d8b095ce5a52b4d8c579b555c17a938e9
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-08-14 16:39:25 +0100

    merge and concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,15 +1,11 @@
 import { List, Map } from 'immutable';
 
 // Concatenates two arrays into an Immutable.js List.
-function concatElements(page1, page2) {
+export function concatElements(page1, page2) {
   return List(page1).concat(List(page2));
 }
 
 // Merges two objects into an Immutable.js List.
-function mergeElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-
-  const margeMap = map1.mergeWith((value1, value2) => value2, map2);
-  return margeMap.valueSeq().toList();
+export function mergeElements(page1, page2) {
+  return Map(page1).concat(Map(page2));
 }
