795072928745e93d4f52edaedbb4f5f91c524f1e
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-08-14 17:02:44 +0100

    Maps Equality By is

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,3 +1,5 @@
 const { is, Map } = require('immutable');
 
-export const areMapsEqual = (map1, map2) => is(Map(map1), Map(map2));
+const areMapsEqual = (map1, map2) => is(Map(map1), Map(map2));
+
+export default areMapsEqual;
