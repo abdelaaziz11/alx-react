04753c70d2833bad494f44782f159b874fc625d3
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-08-14 17:16:30 +0100

    lazy seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,15 +1,15 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(grades) {
-  const bestStudent = Seq(grades)
-    .filter(student => student.get('score') >= 70)
-    .map(student => ({
-      firstName:
-        student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-      lastName:
-        student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
-    }));
-  bestStudent.forEach(student => {
-    console.log(`${student.firstName} ${student.lastName}`);
-  });
+	const bestStudents = Seq(grades)
+		.filter((student) => student.score >= 70)
+		.map((student) => ({
+			score: x.score,
+			firstName:
+				student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+			lastName:
+				student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+		}));
+
+	console.log(bestStudents.toObject());
 }
