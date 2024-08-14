9581771125d5136679352277d92acd642aec9421
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-08-14 16:30:40 +0100

    accssing nested elements

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
 const { getIn, fromJS } = require('immutable');
 
-export default function accessImmutableObject (object, array) {
+export default function accessImmutableObject(object, array) {
   return getIn(fromJS(object), array);
 }
