bce8847d857a74a338827112fad99252c5857737
abdelaaziz11 <140088735+abdelaaziz11@users.noreply.github.com>
2024-08-14 16:44:23 +0100

    Nested merge

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,33 +1,5 @@
-const { Map } = require('immutable');
+import { Map } from 'immutable';
 
-const page1 = {
-  'user-1': {
-    id: 1,
-    name: 'test',
-    likes: {
-      1: {
-        uid: 1234,
-      }
-    }
-  },
-};
-
-const page2 = {
-  'user-1': {
-    likes: {
-      2: {
-        uid: 134,
-      }
-    }
-  },
-};
-
-mergeDeeplyElements(page1, page2).toJS();
-
-function mergeDeeplyElements(page1, page2) {
-    const map1 = Map(page1);
-    const map2 = Map(page2);
-
-    const deepMergeMap = map1.mergeDeep(map2);
-    return deepMergeMap.valueSeq().toList();
+export function mergeDeeplyElements(page1, page2) {
+  Map(page1).mergeDeep(Map(page2));
 }
